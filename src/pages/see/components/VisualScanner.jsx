c2c7ff34d065577384ccf1478eaeb37523a02056
moxx <ilyalakhno@gmail.com>
2024-02-27 19:20:24 +0600

    adapt machine vision layout

--- src/pages/see/components/VisualScanner.jsx
+++ src/pages/see/components/VisualScanner.jsx
@@ -6,9 +6,11 @@ import FaceStats from './FaceStats'
 const VisualScanner = ({ data, stage, facemap, landmarks, mesh }) => {
 
 	return (
-		<div className='tw-flex-1 tw-flex tw-items-center'>
-			<div className='column-1/3 tw-flex tw-flex-col tw-gap-14 tw-justify-center tw-items-between tw-px-16'>
-				{!!stage && data && <FaceStats data={data} />}
+		<div className='tw-flex-1 tw-flex tw-items-center tw-self-stretch'>
+			<div className='column-1/3 tw-flex tw-justify-center'>
+				<div className='tw-max-w-[640px] tw-flex tw-flex-col tw-gap-14 tw-justify-center tw-items-between tw-px-16'>
+					{!!stage && data && <FaceStats data={data} />}
+				</div>
 			</div>
 			<div className='tw-flex-1 tw-flex tw-flex-col tw-self-end tw-items-center tw-pb-10 column-1/3'>
 				<p className='tw-bg-white tw-rounded-full tw-py-3 tw-px-4 tw-flex tw-gap-3'>
@@ -16,8 +18,10 @@ const VisualScanner = ({ data, stage, facemap, landmarks, mesh }) => {
 					<span className='tw-text-xl'>{data?.message || 'Your face must be inside the frame'}</span>
 				</p>
 			</div>
-			<div className='column-1/3 tw-flex tw-flex-col tw-gap-14 tw-justify-center tw-items-between tw-px-16'>
-				{!!stage && data && <FaceScanProgress stage={stage} marks={landmarks} facemap={facemap} mesh={mesh} />}
+			<div className='column-1/3 tw-flex tw-justify-center'>
+				<div className='tw-max-w-[640px] tw-flex tw-flex-col tw-gap-14 tw-justify-center tw-items-between tw-px-16'>
+					{!!stage && data && <FaceScanProgress stage={stage} marks={landmarks} facemap={facemap} mesh={mesh} />}
+				</div>
 			</div>
 		</div>
 	)
